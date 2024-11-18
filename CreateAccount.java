a3449d5a25af11fa3407fd10a3c33ae834501872
Sasirekha-19 <sasijayababu@gmail.com>
2024-11-18 11:53:14 +0530

    Add files via upload

---
+++ CreateAccount.java
@@ -0,0 +1,47 @@
+package salesforce;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+public class CreateAccount {
+
+	public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://login.salesforce.com/");
+        driver.findElement(By.id("username")).sendKeys("gokul.sekar@testleaf.com");
+        driver.findElement(By.id("password")).sendKeys("Leaf@123");
+        driver.findElement(By.id("Login")).click();
+        driver.findElement(By.xpath("//div[@class='slds-icon-waffle']")).click();
+        driver.findElement(By.xpath("//button[text()='View All']")).click();
+        Thread.sleep(5000);
+        WebElement element = driver.findElement(By.xpath("//p[text()='Sales']/ancestor::a"));
+        Thread.sleep(2000);
+        Actions act = new Actions(driver);
+        act.scrollToElement(element).perform();
+        element.click();
+        WebElement ClickAccounts = driver.findElement(By.xpath("//a[@title='Accounts']"));
+        driver.executeScript("arguments[0].click()",ClickAccounts);
+        driver.findElement(By.xpath("//a[@title='New']")).click();
+        String accountName = "Gokul";
+        driver.findElement(By.xpath("//label[text()='Account Name']/following::input")).sendKeys(accountName);
+        WebElement ownershipDD = driver.findElement(By.xpath("//button[@aria-label='Ownership']"));
+        driver.executeScript("arguments[0].click()",ownershipDD);
+        driver.findElement(By.xpath("//span[text()='Public']")).click();
+        driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
+        String toastMessage = driver.findElement(By.xpath("//span[@class='toastMessage slds-text-heading--small forceActionsText']")).getText();
+        System.out.println(toastMessage);
+        Assert.assertTrue(toastMessage.contains(accountName),"Verify the Account name");
+    }
+
+}
